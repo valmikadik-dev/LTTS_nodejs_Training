17cd63dd89a0cc886c04f9c864ebf0b9d9954a8c
valmikadik-dev <72722893+valmikadik-dev@users.noreply.github.com>
2020-10-12 10:57:57 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -4,7 +4,6 @@ upper = 1000
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
